8c31032d6ae41a1798701c746dc3917618877f24
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-05-23 15:22:42 -0500

    added seed and limit to api

--- src/components/utils/API.js
+++ src/components/utils/API.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/?results=5";
-// const APIKEY = "";
+const BASEURL = "https://randomuser.me/api/";
 
-// Export an object with a "search" method that searches the Randomuser API for the passed query
-export default {
-  search: function(query) {
-    return axios.get(BASEURL + query);
-  }
-};
-
+function getUsers(seed, limit) {
+  //Seeds allow you to always generate the same set of users.
+  //Limit sets limit of how many results are returned.
+  return axios.get(BASEURL + "?seed="+seed+"&results="+limit);
+}
 
 //https://randomuser.me/api/?gender=female
 //https://randomuser.me/api/?nat=gb
 //https://randomuser.me/api/?dob=1993-07-20T09:44:18.674Z
 
+export default getUsers;
