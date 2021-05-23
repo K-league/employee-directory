af469b6b838307481fad25db456d80a0d1cc7f28
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-05-23 17:12:02 -0500

    removed nav and main imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import Main from "./components/Main.js";
 import Header from "./components/Header.js";
-import Nav from "./components/Nav.js";
 import DataTable from "./components/DataTable.js"
 import Wrapper from './components/Wrapper';
 //import wrapper header main to render here
